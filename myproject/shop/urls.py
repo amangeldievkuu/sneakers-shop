32e8e275cc6b6c39d8c419a28aa3b80bd78ede38
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-22 15:42:39 +0800

    refactor shop urls

--- myproject/shop/urls.py
+++ myproject/shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('shop/', ProductListView.as_view(), name='product-list'),
-    path('shop/products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
