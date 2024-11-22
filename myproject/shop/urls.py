2f9eb8423e04569e6f89252e809c6ee93c2ebccf
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-22 16:03:34 +0800

    configure urls

--- myproject/shop/urls.py
+++ myproject/shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, CartView
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('api/cart/', CartView.as_view(), name='cart'),
+    path('api/cart/<int:product_id>/', CartView.as_view(), name='cart-delete'),
 ]
--- myproject/sneaker/urls.py
+++ myproject/sneaker/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls
@@ -8,4 +9,6 @@ urlpatterns = router.urls
 urlpatterns += [
     path('admin/', admin.site.urls),
     path('api/', include('shop.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Token generation
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh
 ]
