939ec020f1e38b96c6a59cf763bfafd5890ebb1a
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-22 14:44:26 +0800

    add model for Sneaker product

--- myproject/shop/models.py
+++ myproject/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to="products/")
