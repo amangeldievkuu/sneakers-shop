e67fb64231c20f9410b7f0d451bfc3d037477972
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-22 16:03:15 +0800

    add card view

--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Cart
+from .serializers import ProductSerializer, CartSerializer
 
 # Create your views here.
 
@@ -15,3 +15,20 @@ class ProductListView(generics.ListAPIView):
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class CartView(APIView):
+    def post(self, request):
+        serializer = CartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        carts = Cart.objects.filter(user=request.user)
+        serializer = CartSerializer(carts, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, product_id):
+        Cart.objects.filter(user=request.user, product_id=product_id).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
