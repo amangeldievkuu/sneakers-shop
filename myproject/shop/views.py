fdba4d7ec6141c02297a250141137fa8f7d27c1d
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-22 15:34:07 +0800

    add Views for product-detail and product-list

--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
