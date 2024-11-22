adc607763cbfbb0e18d1c60247923ba8ab54853f
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-22 15:53:08 +0800

    model for Cart

--- myproject/shop/models.py
+++ myproject/shop/models.py
@@ -10,4 +10,14 @@ class Product(models.Model):
     image = models.ImageField(upload_to="products/")
 
     def __str__(self) -> str:
-        return f"name: {self.name}, brand: {self.brand}, price: {self.price}, image: {self.image}"
+        return f"name: {self.name}, brand: {self.brand}, price: {self.price}"
+
+
+class Cart(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+    def __str__(self) -> str:
+        return f"name: {self.product.name}, quantity: {self.quantity}"
